903108a2c0a67fcd824e73a49101389e9ac7675f
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-11-29 10:36:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
-int main(){
-  bool ris;
-  int primo=0, i=1;
-  cin>>primo;
-  ris=primos(primo,i);
-  if (ris==true){
-    cout<<"numero primo";
-  }
-  else{
-      cout<<"numero non primo";
-  }
-  return 0;
+int main() {
+    int numero, conta=0;
+    cin >> numero;
+    primo(numero, 1,conta);
+    if (conta==0){
+        cout << "numero primo";}
+    else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
